539f9d5a226a876489d7af2efa7b78712349886a
Adi Primadharma <adhie.scythe@gmail.com>
2019-11-09 09:56:32 +0700

    add animated bash 9

--- rsync.sh
+++ rsync.sh
@@ -1,6 +1,6 @@
 rsync -avzP --rsh=ssh /Users/adi/HTML/wedding/ root@23.94.5.247:/var/www/html/
 
-echo ================== Finish Sync ==================
+echo ====================== Finish Sync =====================
 c1()
 {
     echo "           _^____      "
