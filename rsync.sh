d3e6caee11043a0099ca24d5ad5e609c11e93957
Adi Primadharma <adhie.scythe@gmail.com>
2019-11-09 09:43:21 +0700

    add animated bash 3

--- rsync.sh
+++ rsync.sh
@@ -4,10 +4,10 @@ echo ================== Finish Sync ==================
 bar="=================================================="
 barlength=${#bar}
 i=0
-while ((i < 100)); do
+while ((i < 5)); do
   # Number of bar segments to draw.
-  n=$((i*barlength / 100))
-  printf "\r[%-${barlength}s]" "${bar:0:n}"
+  n=$((i*barlength / 5))
+  printf "\r%-${barlength}s" "${bar:0:n}"
   ((i += RANDOM%5+2))
   # Of course, in real life, we'd be getting i from somewhere meaningful.
   sleep 1
