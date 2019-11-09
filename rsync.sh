be240774b1f6f7ab4b8a1e6d585a6714170963b1
Adi Primadharma <adhie.scythe@gmail.com>
2019-11-09 09:38:59 +0700

    add animated bash 2

--- rsync.sh
+++ rsync.sh
@@ -1,12 +1,16 @@
 rsync -avzP --rsh=ssh /Users/adi/HTML/wedding/ root@23.94.5.247:/var/www/html/
 
 echo ================== Finish Sync ==================
-chars="/-\|"
-
-while :; do
-  for (( i=0; i<${#chars}; i++ )); do
-    sleep 0.5
-    echo -en "${chars:$i:1}" "\r"
-  done
+bar="=================================================="
+barlength=${#bar}
+i=0
+while ((i < 100)); do
+  # Number of bar segments to draw.
+  n=$((i*barlength / 100))
+  printf "\r[%-${barlength}s]" "${bar:0:n}"
+  ((i += RANDOM%5+2))
+  # Of course, in real life, we'd be getting i from somewhere meaningful.
+  sleep 1
 done
+echo
 echo ================== Executing Git Push ==================
