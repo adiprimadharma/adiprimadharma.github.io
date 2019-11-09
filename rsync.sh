dd78a7b2faee79f834c90ee6b283c4c944aca136
Adi Primadharma <adhie.scythe@gmail.com>
2019-11-09 09:37:00 +0700

    add animated bash

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,12 @@
 rsync -avzP --rsh=ssh /Users/adi/HTML/wedding/ root@23.94.5.247:/var/www/html/
 
-echo ================== Finish Sync ==================\n\n
+echo ================== Finish Sync ==================
+chars="/-\|"
+
+while :; do
+  for (( i=0; i<${#chars}; i++ )); do
+    sleep 0.5
+    echo -en "${chars:$i:1}" "\r"
+  done
+done
 echo ================== Executing Git Push ==================
