e9dd907d52b8f26e2af6d53bec530395a16cb3ca
Adi Primadharma <adhie.scythe@gmail.com>
2019-11-09 09:45:38 +0700

    add animated bash 4

--- rsync.sh
+++ rsync.sh
@@ -1,16 +1,39 @@
 rsync -avzP --rsh=ssh /Users/adi/HTML/wedding/ root@23.94.5.247:/var/www/html/
 
 echo ================== Finish Sync ==================
-bar="=================================================="
-barlength=${#bar}
-i=0
-while ((i < 5)); do
-  # Number of bar segments to draw.
-  n=$((i*barlength / 5))
-  printf "\r%-${barlength}s" "${bar:0:n}"
-  ((i += RANDOM%5+2))
-  # Of course, in real life, we'd be getting i from somewhere meaningful.
-  sleep 1
+c1()
+{
+    echo "           _^____      "
+    echo "   L    __/    []\\     "
+    echo "  LOL===_         \\    "
+    echo "   L     \\_________]   "
+    echo "           I   I       "
+    echo "          -------/     "
+    sleep 0.1
+}
+c2()
+{
+    echo "           _^____      "
+    echo "  L L   __/    []\\     "
+    echo "   O====_         \\    "
+    echo "  L L    \\_________]   "
+    echo "           I   I       "
+    echo "          -------/     "
+    sleep 0.1
+}
+echo -e "\n\n\n\n\n\n\n\n\n\033[8F\033[s"
+while [ 1 = 1 ]
+do  echo -e "\033[u   ROFL:ROFL:ROFL:ROFL " ; c1
+    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
+    echo -e "\033[u        ROFL:ROFL      " ; c1
+    echo -e "\033[u           LOL         " ; c2
+    echo -e "\033[u        ROFL:ROFL      " ; c1
+    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
+    echo -e "\033[u   ROFL:ROFL:ROFL:ROFL " ; c1
+    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
+    echo -e "\033[u        ROFL:ROFL      " ; c1
+    echo -e "\033[u           LOL         " ; c2
+    echo -e "\033[u        ROFL:ROFL      " ; c1
+    echo -e "\033[u     LOL:LOL:LOL:LOL   " ; c2
 done
-echo
 echo ================== Executing Git Push ==================
